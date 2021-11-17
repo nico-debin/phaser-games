19b5fbd4e7ea9142110fe019cfb83192188bc575
Nicolas De Bin <10942950+nicodebin@users.noreply.github.com>
2021-11-18 00:45:22 +0100

    feat(crawler-dungeon): lizard enemy

--- src/dungeon-crawler/src/scenes/Game.ts
+++ src/dungeon-crawler/src/scenes/Game.ts
@@ -1,5 +1,10 @@
 import Phaser from 'phaser'
 
+import { createLizardAnims } from '../anims/EnemyAnims'
+import { createCharacterAnims } from '../anims/CharacterAnims'
+import { debugDraw } from '../utils/debug'
+
+import Lizard from '../enemies/Lizard'
 export default class Game extends Phaser.Scene {
   private cursors!: Phaser.Types.Input.Keyboard.CursorKeys
   private faune!: Phaser.Physics.Arcade.Sprite
@@ -13,6 +18,9 @@ export default class Game extends Phaser.Scene {
   }
 
   create() {
+    createCharacterAnims(this.anims)
+    createLizardAnims(this.anims)
+
     const map = this.make.tilemap({
       key: 'dungeon',
     })
@@ -22,88 +30,29 @@ export default class Game extends Phaser.Scene {
     const wallsLayer = map.createLayer('Walls', tileset)
 
     wallsLayer.setCollisionByProperty({ collides: true })
-
-    const debugGraphics = this.add.graphics().setAlpha(0.7)
-    wallsLayer.renderDebug(debugGraphics, {
-      tileColor: null,
-      collidingTileColor: new Phaser.Display.Color(243, 234, 48, 255),
-      faceColor: new Phaser.Display.Color(40, 39, 37, 255),
-    })
+    // debugDraw(wallsLayer, this)
 
     this.faune = this.physics.add.sprite(128, 128, 'faune', 'walk-down-3.png')
     this.faune.body.setSize(this.faune.width * 0.5, this.faune.height * 0.8)
 
-    this.anims.create({
-      key: 'faune-idle-down',
-      frames: [
-        {
-          key: 'faune',
-          frame: 'walk-down-3.png',
-        },
-      ],
-    })
-
-    this.anims.create({
-      key: 'faune-idle-up',
-      frames: [
-        {
-          key: 'faune',
-          frame: 'walk-up-3.png',
-        },
-      ],
-    })
-
-    this.anims.create({
-      key: 'faune-idle-side',
-      frames: [
-        {
-          key: 'faune',
-          frame: 'walk-side-3.png',
-        },
-      ],
-    })
-
-    this.anims.create({
-      key: 'faune-run-down',
-      frames: this.anims.generateFrameNames('faune', {
-        start: 1,
-        end: 8,
-        prefix: 'run-down-',
-        suffix: '.png',
-      }),
-      repeat: -1,
-      frameRate: 15,
-    })
-
-    this.anims.create({
-      key: 'faune-run-up',
-      frames: this.anims.generateFrameNames('faune', {
-        start: 1,
-        end: 8,
-        prefix: 'run-up-',
-        suffix: '.png',
-      }),
-      repeat: -1,
-      frameRate: 15,
-    })
-
-    this.anims.create({
-      key: 'faune-run-side',
-      frames: this.anims.generateFrameNames('faune', {
-        start: 1,
-        end: 8,
-        prefix: 'run-side-',
-        suffix: '.png',
-      }),
-      repeat: -1,
-      frameRate: 15,
-    })
-
-    this.faune.anims.play('faune-down-side')
-
-    this.physics.add.collider(this.faune, wallsLayer)
+    this.faune.anims.play('faune-idle-down')
 
     this.cameras.main.startFollow(this.faune, true)
+
+    const lizards = this.physics.add.group({
+      classType: Lizard,
+      createCallback: (gameObject: Phaser.GameObjects.GameObject) => {
+        const lizardGameObject = gameObject as Lizard
+        lizardGameObject.body
+          .setSize(lizardGameObject.width, lizardGameObject.height * 0.6)
+          .setOffset(0, 10)
+        lizardGameObject.body.onCollide = true
+      },
+    })
+    lizards.get(256, 128, 'lizard')
+
+    this.physics.add.collider(this.faune, wallsLayer)
+    this.physics.add.collider(lizards, wallsLayer)
   }
 
   update(t: number, dt: number) {
@@ -137,7 +86,7 @@ export default class Game extends Phaser.Scene {
         parts[1] = 'idle'
         this.faune.anims.play(parts.join('-'))
       } else {
-        this.faune.anims.play('faine-idle-down')
+        this.faune.anims.play('faune-idle-down')
       }
 
       this.faune.setVelocity(0, 0)
