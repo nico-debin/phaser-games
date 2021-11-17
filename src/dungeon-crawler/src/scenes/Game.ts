9e237e9aaf124a51c1e368cba083c9f4765cf2b3
Nicolas De Bin <10942950+nicodebin@users.noreply.github.com>
2021-11-17 20:55:05 +0100

    fix: tile bleeding with tile extruder

--- src/dungeon-crawler/src/scenes/Game.ts
+++ src/dungeon-crawler/src/scenes/Game.ts
@@ -16,7 +16,7 @@ export default class Game extends Phaser.Scene {
     const map = this.make.tilemap({
       key: 'dungeon',
     })
-    const tileset = map.addTilesetImage('dungeon', 'tiles')
+    const tileset = map.addTilesetImage('dungeon', 'tiles', 16, 16, 1, 2)
 
     map.createLayer('Ground', tileset)
     const wallsLayer = map.createLayer('Walls', tileset)
