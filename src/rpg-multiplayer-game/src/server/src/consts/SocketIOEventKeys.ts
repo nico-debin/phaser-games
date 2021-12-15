ee03a688cc5cbf3c258a2f6557c8fc57d77b4e29
Nicolas De Bin <10942950+nicodebin@users.noreply.github.com>
2021-12-15 20:57:58 +0100

    chore: small code arrangement

---
+++ src/rpg-multiplayer-game/src/server/src/consts/SocketIOEventKeys.ts
@@ -0,0 +1,9 @@
+enum SocketIOEventKeys {
+  // A new client has connected to the socket
+  Connection = 'connection',
+
+  // A client has disconnected from the socket
+  Disconnect = 'disconnect',
+}
+
+export default SocketIOEventKeys
--- src/rpg-multiplayer-game/src/server/src/scenes/Game.ts
+++ src/rpg-multiplayer-game/src/server/src/scenes/Game.ts
@@ -1,20 +1,28 @@
 import Phaser from 'phaser'
 import { Server } from 'socket.io'
 
+import settings from '~/settings'
+
 import NetworkEventKeys from '../consts/NetworkEventKeys'
 import SceneKeys from '../consts/SceneKeys'
 import TextureKeys from '../consts/TextureKeys'
 
-import { MovementInput, PlayerId, PlayersStates, PlayerState } from '../types/playerTypes'
+import {
+  MovementInput,
+  PlayerId,
+  PlayersStates,
+  PlayerState,
+} from '../types/playerTypes'
 
 import Player from '../characters/Player'
+import SocketIOEventKeys from '~/consts/SocketIOEventKeys'
 declare global {
   interface Window {
     io: Server
   }
 }
 
-const io = window.io || {on: () => {}}
+const io = window.io || { on: () => {} }
 export default class Game extends Phaser.Scene {
   // Phaser representation of the players
   players!: Phaser.Physics.Arcade.Group
@@ -29,22 +37,52 @@ export default class Game extends Phaser.Scene {
   }
 
   create() {
+    this.createPlayers()
+    this.createTilesets()
+    this.createSocketHandlers()
+  }
+
+  private createPlayers() {
+    this.players = this.physics.add.group({
+      classType: Player,
+    })
+    this.physics.add.collider(this.players, this.players)
+  }
+
+  private createTilesets() {
     this.mapIsland = this.make.tilemap({
       key: 'islands',
     })
 
     // Tilset are created in the server to handle collisions
-    const tilesetIslandBeach = this.mapIsland.addTilesetImage('tf_beach_tileB', 'tiles-islands-beach', 32, 32, 0, 0)
-    const tilesetIslandShoreline = this.mapIsland.addTilesetImage('tf_beach_tileA1', 'tiles-islands-shoreline', 32, 32, 0, 0)
+    const tilesetIslandBeach = this.mapIsland.addTilesetImage(
+      'tf_beach_tileB',
+      'tiles-islands-beach',
+      32,
+      32,
+      0,
+      0,
+    )
+    const tilesetIslandShoreline = this.mapIsland.addTilesetImage(
+      'tf_beach_tileA1',
+      'tiles-islands-shoreline',
+      32,
+      32,
+      0,
+      0,
+    )
 
-    const islandLayer = this.mapIsland.createLayer('Island 1/Island', [tilesetIslandBeach, tilesetIslandShoreline])
-    const pathsLayer = this.mapIsland.createLayer('Island 1/Paths', [tilesetIslandBeach])
-    const vegetationBottomLayer = this.mapIsland.createLayer('Island 1/Vegetation bottom', tilesetIslandBeach)
-
-    this.players = this.physics.add.group({
-      classType: Player,
-    })
-    this.physics.add.collider(this.players, this.players)
+    const islandLayer = this.mapIsland.createLayer('Island 1/Island', [
+      tilesetIslandBeach,
+      tilesetIslandShoreline,
+    ])
+    const pathsLayer = this.mapIsland.createLayer('Island 1/Paths', [
+      tilesetIslandBeach,
+    ])
+    const vegetationBottomLayer = this.mapIsland.createLayer(
+      'Island 1/Vegetation bottom',
+      tilesetIslandBeach,
+    )
 
     // Tileset colliders
     islandLayer.setCollisionByProperty({ collides: true })
@@ -52,18 +90,27 @@ export default class Game extends Phaser.Scene {
     vegetationBottomLayer.setCollisionByProperty({ collides: true })
 
     // Collide players with tiles
-    this.physics.add.collider(this.players, [islandLayer, pathsLayer, vegetationBottomLayer])
+    this.physics.add.collider(this.players, [
+      islandLayer,
+      pathsLayer,
+      vegetationBottomLayer,
+    ])
+  }
 
+  private createSocketHandlers() {
     const gameScene: Game = this
 
-    io.on('connection', (socket) => {
+    io.on(SocketIOEventKeys.Connection, (socket) => {
       const playerId: PlayerId = socket.id
 
       console.log(`user ${playerId} connected`)
 
       // Get player initial location from object layer
       const playersLayer = this.mapIsland.getObjectLayer('Players')
-      const randomPlayerIndex = Phaser.Math.Between(0, playersLayer.objects.length - 1)
+      const randomPlayerIndex = Phaser.Math.Between(
+        0,
+        playersLayer.objects.length - 1,
+      )
       const playerObj = playersLayer.objects[randomPlayerIndex]
 
       // create a new player and add it to our players object
@@ -86,24 +133,18 @@ export default class Game extends Phaser.Scene {
       socket.emit(NetworkEventKeys.PlayersInitialStatusInfo, this.playersStates)
 
       // update all other players of the new player
-      socket.broadcast.emit(NetworkEventKeys.PlayersNew, this.playersStates[playerId])
+      socket.broadcast.emit(
+        NetworkEventKeys.PlayersNew,
+        this.playersStates[playerId],
+      )
 
       // Player disconnected handler
-      socket.on('disconnect', function () {
-        console.log(`user ${playerId} disconnected`)
-
-        // remove player from server
-        removePlayer(gameScene, playerId)
-
-        // remove this player from our players object
-        delete gameScene.playersStates[playerId]
-
-        // emit a message to all players to remove this player
-        io.emit(NetworkEventKeys.PlayersLeft, playerId)
-      })
+      socket.on(SocketIOEventKeys.Disconnect, () => handleSocketDisconnect(gameScene, playerId))
 
       // Player input handler: when a player moves, update the player data
-      socket.on(NetworkEventKeys.PlayersInput, function (inputData: MovementInput) {
+      socket.on(NetworkEventKeys.PlayersInput, function (
+        inputData: MovementInput,
+      ) {
         handlePlayerInput(gameScene, playerId, inputData)
       })
     })
@@ -118,7 +159,9 @@ export default class Game extends Phaser.Scene {
       player.update(input)
 
       if (!playerHasMoved) {
-        playerHasMoved = this.playersStates[player.id].x != player.x || this.playersStates[player.id].y != player.y
+        playerHasMoved =
+          this.playersStates[player.id].x != player.x ||
+          this.playersStates[player.id].y != player.y
       }
 
       this.playersStates[player.id].x = player.x
@@ -155,11 +198,29 @@ const removePlayer = (scene: Game, playerId: PlayerId) => {
   })
 }
 
-const handlePlayerInput = (scene: Game, playerId: PlayerId, input: MovementInput) => {
+const handlePlayerInput = (
+  scene: Game,
+  playerId: PlayerId,
+  input: MovementInput,
+) => {
   scene.players.getChildren().forEach((gameObject) => {
     const player = gameObject as Player
     if (playerId === player.id) {
       scene.playersStates[player.id].movementInput = input
     }
   })
+}
+
+/*** Socket Handlers ***/
+const handleSocketDisconnect = (gameScene: Game, playerId: PlayerId) => {
+  console.log(`user ${playerId} disconnected`)
+
+  // remove player from server
+  removePlayer(gameScene, playerId)
+
+  // remove this player from our players object
+  delete gameScene.playersStates[playerId]
+
+  // emit a message to all players to remove this player
+  io.emit(NetworkEventKeys.PlayersLeft, playerId)
 }
