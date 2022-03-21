022bb9d464331029d8c54fe86eccb79ced75eba3
Nicolas De Bin <10942950+nicodebin@users.noreply.github.com>
2022-03-21 21:43:54 +0100

    fix: port fix

--- src/rpg-multiplayer-game/src/index.ts
+++ src/rpg-multiplayer-game/src/index.ts
@@ -5,7 +5,7 @@ import { Server } from 'socket.io'
 import { JSDOM, VirtualConsole } from 'jsdom'
 import DatauriParser from 'datauri/parser'
 
-const PORT = process.env.NODE_ENV ? 80 : process.env.PORT || 8081
+const PORT = process.env.PORT || 8081
 
 const app: Application = express()
 const httpServer = new HttpServer(app)
