000111796993ac0305d3813ab76963c7390a495d
Nicolas De Bin <10942950+nicodebin@users.noreply.github.com>
2022-05-03 19:12:30 +0200

    feat: learning break session 1 initial setup

---
+++ src/learning-break/1-sesion-1/src/consts/index.ts
@@ -0,0 +1,2 @@
+export { default as SceneKeys } from './SceneKeys';
+export { default as TextureKeys } from './TextureKeys';
---
+++ src/learning-break/1-sesion-1/src/scenes/Preloader.ts
@@ -0,0 +1,19 @@
+import Phaser from "phaser";
+import { SceneKeys, TextureKeys } from "../consts";
+
+export default class Preloader extends Phaser.Scene {
+  constructor() {
+    super({ key: SceneKeys.Preloader });
+  }
+
+  preload() {
+    this.load.image(TextureKeys.LOGO, "./finderlogo.png");
+
+    this.load.image(TextureKeys.SKY, "https://labs.phaser.io/assets/skies/space3.png");
+    this.load.image(TextureKeys.RED, "https://labs.phaser.io/assets/particles/red.png");
+  }
+
+  create() {
+    this.scene.start(SceneKeys.HelloWorld);
+  }
+}
